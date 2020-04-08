31167d42367f7e961cad8dbd3876b17e2a9f53ff
jzheng6182 <jzheng6182@bths.edu>
2020-04-08 12:47:09 -0400

    Adding Pycharm Project

--- venv/Scripts/GUI.py
+++ venv/Scripts/GUI.py
@@ -21,19 +21,29 @@ label3=tk.Label(root, text="Name of Mutated Software:", anchor='w', font =('fira
 label3.place(relx=0, rely=0.5, relwidth=0.45, relheight=0.15)
 entry3 = tk.Entry(root, font=40)
 entry3.place(relx=.5, rely=0.5, relwidth=0.45, relheight=0.09)
-button=tk.Button(root, text="MEWTIFY", bg= "purple", font =40)
+
+entry2.get()
+entry3.get()
+def x():
+    label4 = tk.Label(root, text="Please give a valid python file with the full path",fg = 'red')
+    label4.place(relx=0.5, rely=0.7, relwidth=0.45, relheight=0.05)
+
+button=tk.Button(root, text="MEWTIFY", bg= "purple", font =40,command=x())
 button.place(relx=0.3, rely=0.8, relwidth=0.45, relheight=0.15)
 r = IntVar()
 rbutton=tk.Radiobutton(root, text="option 2", variable=r, value=1).pack(side="right")
 rbutton2=tk.Radiobutton(root, text="option 1", variable=r, value=2).pack(side="right")
+
+
 
 # label6=tk.Label(root, text="MEWTIFIED! Please check the same folder for the file.",fg = 'green')
 # label6.place(relx=0.3, rely=0.6, relwidth=0.45, relheight=0.15)
 
 # label4 = tk.Label(root, text="Please give a valid python file with the full path",fg = 'red')
 # label4.place(relx=0.5, rely=0.7, relwidth=0.45, relheight=0.05)
-# #
+
 # label5 = tk.Label(root, text="Please give a valid name", fg='red')
 # label5.place(relx=0.5, rely=0.6, relwidth=0.45, relheight=0.05)
 
 root.mainloop()
+
