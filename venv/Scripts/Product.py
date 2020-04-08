139454618044f5e13c282240650ec14c02432171
jzheng6182 <jzheng6182@bths.edu>
2020-04-08 12:45:14 -0400

    Adding Pycharm Project

--- venv/Scripts/Product.py
+++ venv/Scripts/Product.py
@@ -43,30 +43,39 @@ def main():
     stub_file = open(stub_name, "w")
     stub_file.write(stub)
     stub_file.close()
-main()
 
+#GUI Dimensions
 HEIGHT = 500
 WIDTH = 700
 root = tk.Tk()
+#GUI NAME AND SIZE
 root.title("MEWTIFY")
 canvas = tk.Canvas(root, height=HEIGHT, width=WIDTH)
 canvas.pack()
 root.resizable(False,False)
+#GUI HEADER
 frame = tk.Frame(root, bg='#80C1FF' , bd=5)
 frame.place(relx=0.5, rely=0.05, relwidth=1, relheight=0.2, anchor='n')
 label=tk.Label(frame,text = "Welcome to Mewtify!", font=("-weight bold",27),bg='#80C1FF')
 label.place(relx=0.3, rely=0, relwidth=.5, relheight=1)
+#INPUT 1
 label2=tk.Label(root, text="Malicous Software with full path:", anchor='w', font = 15)
 label2.place(relx=0, rely=0.35, relwidth=0.47, relheight=0.15)
 entry2 = tk.Entry(root, font=40)
 entry2.place(relx=.5, rely=0.35, relwidth=0.45, relheight=0.09)
+#INPUT2
 label3=tk.Label(root, text="Name of Mutated Software:", anchor='w', font = 15)
 label3.place(relx=0, rely=0.5, relwidth=0.45, relheight=0.15)
 entry3 = tk.Entry(root, font=40)
 entry3.place(relx=.5, rely=0.5, relwidth=0.45, relheight=0.09)
+#GRABBING INPUTS
+input1=entry2.get()
+input2=entry3.get()
+#button mashing
 button=tk.Button(root, text="MEWTIFY", bg= "purple", font =40)
 button.place(relx=0.3, rely=0.8, relwidth=0.45, relheight=0.15)
 r = IntVar()
+#options button
 rbutton=tk.Radiobutton(root, text="option 2", variable=r, value=1).pack(side="right")
 rbutton2=tk.Radiobutton(root, text="option 1", variable=r, value=2).pack(side="right")
 
