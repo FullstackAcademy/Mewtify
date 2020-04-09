913d5c36c8fe83af9ceb9c1a314322269567ad3f
jzheng6182 <60156978+jzheng6182@users.noreply.github.com>
2020-04-09 15:48:06 -0400

    1

--- venv/venv/Scripts/GUI.py
+++ venv/venv/Scripts/GUI.py
@@ -18,7 +18,7 @@ frame.place(relx=0.5, rely=0.05, relwidth=1, relheight=0.2, anchor='n')
 label = tk.Label(frame, text="Welcome to Mewtify!", font=("-weight bold", 27), bg='#80C1FF')
 label.place(relx=0.3, rely=0, relwidth=.5, relheight=1)
 # INPUT 1
-label2 = tk.Label(root, text="Malicous Software with full path:", anchor='w', font=('fira code', 15))
+label2 = tk.Label(root, text="Malicious Software with full path:", anchor='w', font=('fira code', 15))
 label2.place(relx=0, rely=0.35, relwidth=0.47, relheight=0.15)
 entry2 = tk.Entry(root, font=40)
 entry2.place(relx=.5, rely=0.35, relwidth=0.45, relheight=0.09)
